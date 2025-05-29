0453159654fec15a596b7912e1e566cb5b092007
Diyorbek <erkinovdiyor2410@gmail.com>
2025-05-29 16:33:02 +0500

    zo'r

--- frontend-resturant/js/script.js
+++
@@ -1,9 +0,0 @@
-import form from './modules/form.js'
-import headerMenu from './modules/headerMenu.js'
-import orderRoom from './modules/orderRoom.js'
-
-document.addEventListener('DOMContentLoaded', () => {
-	headerMenu()
-	orderRoom()
-	form()
-})
